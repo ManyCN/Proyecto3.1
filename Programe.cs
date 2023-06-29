7788f2307ba218ef0642b0710e8a10bfb787c7c0
ManyCN <manycolnav6937@gmail.com>
2023-06-29 10:19:20 -0600

    Copia 2 de la copia

--- Programe.cs
+++ Programe.cs
@@ -8,5 +8,28 @@ namespace Primer_Proyecto
 {
     internal class Programe
     {
+        static void Main(string[] args)
+        {
+
+            // Creamos un objeto coche
+            Coche c = new Coche(1, "BMW", "4", 100, 12000);
+
+            // get de precio de coche
+            Console.WriteLine(c.precio);
+
+            // get de marca de coche
+            Console.WriteLine(c.marca);
+
+            // Modifico el precio del coche con el set
+            c.precio = 15000;
+
+            // Vuelvo a mostrar para ver si se ha modificado
+            Console.WriteLine(c.precio);
+
+            // Muestro el estado completo
+            Console.WriteLine(c.ToString());
+
+            Console.ReadLine();
+        }
     }
 }
